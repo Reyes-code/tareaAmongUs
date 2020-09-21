189b304a8dec28837be52da53b045028eeb2228a
Santiago1707 <reyespiraligua29@gmail.com>
2020-09-20 19:00:29 -0500

    Tarea Among Us Carlos Reyes

---
+++ js/script.js
@@ -0,0 +1,175 @@
+function rosadoOsito() {
+    let imposto= true
+
+    if (imposto == true) {
+        alert("Tripulante: Esta recargando la gasolina del motor");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function cafeOsito() {
+    let tripu= true
+
+    if (tripu== true) {
+        alert ("Tripulante: Esta revisando cámaras")
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function azulPez() {
+    let impo= false
+
+    if (impo == !true) {
+        alert("Tripulante: Esta en enfermeria escaneandose ");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function azulPing() {
+    let trip= true
+
+    if (trip == true) {
+        alert("Tripulante: Esta dirigiendo la nave");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function blancoPerro() {
+    let impostor= true
+    
+    if (impostor == true) {
+        alert("Impostor: No se lo digas a nadie!");
+    } else {
+        alert("Tripulante: Caminando por los pasillos")
+    }
+}
+
+function negroPajaro() {
+    let trip= true
+
+    if (trip == true) {
+        alert("Tripulante: Escaneando muestras");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function verdeSapo() {
+    let tripul=  true
+
+    if (tripul == true) {
+        alert("Tripulante: Vaciando basurero en almacén");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function rosadoFlores() {
+    let tripula= true
+
+    if (tripula == true) {
+        alert("Tripulante: Usando tarjeta en administración");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function rosadoEstrella() {
+    let tripulan= true
+
+    if (tripulan== true) {
+        alert("Tripulante: Arreglando la luz en electricidad" );
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function azulEstrella() {
+    let tripulant= true
+
+    if (tripulant== true) {
+        alert("Tripulante: Descargando datos de cafeteria");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function naranjaHuevo() {
+    let impostor2 = true
+
+    if (impostor2== true) {
+        alert("Impostor: No se lo digas a nadie!");
+    } else {
+        alert("Tripulante: Revisando camaras");
+    }
+}
+function amarilloPerro() {
+    let tripulante2 = true
+
+    if (tripulante2 == true) {
+        alert("Tripulante: Esta caminando por los pasillos");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function blancoOrejas() {
+    let impostor3= false
+
+    if (impostor3 == !false) {
+        alert("Tripulante: Arreglando conexiones en Escudos");
+    } else {
+        alert("Impostor: No se lo digas a nadie!"); 
+    }
+}
+
+function negroVampiro() {
+    let tripulante3=  true
+
+    if (tripulante3= true) {
+        alert("Tripulate: Reparando el oxigeno")
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function rosadoConejo() {
+    let tripulante4 = true
+
+    if (tripulante4 == true) {
+        alert("Tripulante: Reparando el reactor");
+    } else {
+        alert("Impostor: No se lo digas a nadie!");
+    }
+}
+
+function rojoGato() {
+    let tripulante5 = true
+
+    if (tripulante5 == !true) {
+        alert("Impostor: No se lo digas a nadie!");
+    } else {
+        alert("Tripulante: Subiendo Datos");
+    }
+}
+
+function final() {
+    alert("...");
+}
+
+function fantasma() {
+    alert("Fantasma: Debe terminar sus tareas");
+}
+
+function muerto() {
+    alert("Han matado a alguien, REPORTALO!!");
+}
+
+
+/* El blanco con orejas
+el del huevito
+blanco perrito */
